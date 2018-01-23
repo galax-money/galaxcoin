0122259cd7da8463bd371e9f3ff8adaf4ed9287b
B4dT0bi <b4dt0bi@vps3674063.delta-networks.de>
2018-01-23 14:30:20 +0100

    change node

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -95,7 +95,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "46.4.36.66:22031", 
-  "188.40.59.80:22031",
+  "134.255.237.190:22031",
 };
 
 struct CheckpointData {
