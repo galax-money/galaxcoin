e05e29975074bdf1602f2ffa5c92b1810b44f37f
Tobi <tobias.boese@gmail.com>
2018-01-29 13:10:23 +0100

    add blockchain explorer rpc functions

--- external/rocksdb/util/build_version.cc
+++ external/rocksdb/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:a57e0fe343c602ed60cf2bdcb9e7b00fa915f171";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2018-01-28";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:97fc5b84b89ed7d37e522f1ec349266e5b8d4b76";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2018-01-29";
 const char* rocksdb_build_compile_date = __DATE__;
