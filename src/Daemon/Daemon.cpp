4601be9753acf3333914e8e8c977187733f4f4a8
Tobi <tobias.boese@gmail.com>
2018-01-29 14:10:28 +0100

    enable cors

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -114,6 +114,7 @@ int main(int argc, char* argv[])
     command_line::add_arg(desc_cmd_sett, arg_log_file);
     command_line::add_arg(desc_cmd_sett, arg_log_level);
     command_line::add_arg(desc_cmd_sett, arg_console);
+    command_line::add_arg(desc_cmd_sett, arg_enable_cors);
     command_line::add_arg(desc_cmd_sett, arg_testnet_on);
 
     RpcServerConfig::initOptions(desc_cmd_sett);
